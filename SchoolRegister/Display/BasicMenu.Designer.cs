9f9887cca8855d453e4bbf368e134ab02f503718
Kalifer738 <47210793+Kalifer738@users.noreply.github.com>
2020-04-09 13:42:01 +0300

    Added UserControlLibrary for the Display

---
+++ SchoolRegister/Display/BasicMenu.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Display
+{
+    partial class BasicMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BasicMenu));
+            this.studentBasicViewer1 = new UserControlsLibrary.StudentBasicViewer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // studentBasicViewer1
+            // 
+            this.studentBasicViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.studentBasicViewer1.Location = new System.Drawing.Point(86, -1);
+            this.studentBasicViewer1.Name = "studentBasicViewer1";
+            this.studentBasicViewer1.Size = new System.Drawing.Size(715, 452);
+            this.studentBasicViewer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 34);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BasicMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.studentBasicViewer1);
+            this.Name = "BasicMenu";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControlsLibrary.StudentBasicViewer studentBasicViewer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
--- SchoolRegister/Display/BasicMenu.cs
+++ SchoolRegister/Display/BasicMenu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Display
 {
-    public partial class Form1 : Form
+    public partial class BasicMenu : Form
     {
-        public Form1()
+        public BasicMenu()
         {
             InitializeComponent();
         }
